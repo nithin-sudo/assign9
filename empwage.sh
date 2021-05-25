b2fc049afcbfb95396dc4c0754060cf95c86ef87
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-05-25 18:28:42 +0530

    added employee wage for parttime also

--- empwage.sh
+++ empwage.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -x
 echo "employee wage computaion"
-random=$((RANDOM%2))
+random=$((RANDOM%3))
 wage_perHr=20
-
 case $random in
 	1)echo "employee is present"
 	day_hr=8
 		;;
-	0) echo "employee is absent"
+	2)echo "employee parttime present"
+	day_hr=4
+		;;
+	*) echo "employee is absent"
 	day_hr=0
 		;;
 esac
