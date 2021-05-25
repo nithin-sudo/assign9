fe819bdb75899e27797723f424759cc21298bca9
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-05-25 18:22:59 +0530

    daily wage

--- empwage.sh
+++ empwage.sh
@@ -1,9 +1,15 @@
 #!/bin/bash -x
 echo "employee wage computaion"
 random=$((RANDOM%2))
+wage_perHr=20
+
 case $random in
 	1)echo "employee is present"
+	day_hr=8
 		;;
 	0) echo "employee is absent"
+	day_hr=0
 		;;
 esac
+wage_calc=$(($wage_perHr*$day_hr))
+echo "salary is : $wage_calc"
