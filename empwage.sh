efe5a2051f988a9aeffb33358c52993c8a2a4c07
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-05-25 20:34:43 +0530

    pushing working hrs

--- empwage.sh
+++ empwage.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 echo "employee wage computaion"
+wrkhrs=0
 isPresent=1
 isPartPresent=2
 wage_per_hr=20
@@ -9,20 +10,28 @@ month=20
 empwage=0
 for ((i=1; i<=$month; i++))
 do
-	random=$((RANDOM%3))
-	case $random in
-		$isPresent)echo "employee is full time present
-		salary=$(($wage_per_hr*$day_hr))
-		empwage=$(($empwage+$salary))
-			;;
-		$isPartPresent)echo "employee parttime present"
-		salary=$(($wage_per_hr*$day_hr))
-                empwage=$(($empwage+$salary))
-			;;
-		*) echo "employee is absent"
-		salary=0
-		empwage=$(($empwage+$salary))
-			;;
-	esac
+	if [ $wrkhrs -lt 100 ]
+	then
+		random=$((RANDOM%3))
+		case $random in
+			$isPresent)echo "employee is full time present"
+			salary=$(($wage_per_hr*$day_hr))
+			empwage=$(($empwage+$salary))
+			wrkhrs=$(($wrkhrs+$day_hr))
+				;;
+			$isPartPresent)echo "employee parttime present"
+			salary=$(($wage_per_hr*$day_hr))
+                	empwage=$(($empwage+$salary))
+			wrkhrs=$(($wrkhrs+$part_day_hr))
+				;;
+			*) echo "employee is absent"
+			salary=0
+			empwage=$(($empwage+$salary))
+				;;
+		esac
+	else
+		break;
+	fi
 done
-echo "salary for a month :$empwage"
+echo "salary credited $empwage rupees for $wrkhrs hrs  in $(($i-1)) days"
+
